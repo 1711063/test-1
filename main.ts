92b694abb29782cc7b9c407dc56067ee805b98e1
1711063 <104405364+1711063@users.noreply.github.com>
2022-04-26 10:19:01 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+    basic.showString("please finsh your homework, thx")
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Angry)
+    basic.showString("please finsh your homework!!!!!")
+})
+basic.showString("hi")
